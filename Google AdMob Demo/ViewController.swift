837caf18c0b490729939721994db2861704b4e2b
Pankajr <pankaj.ray@ecanarys.net>
2019-01-02 15:16:55 +0530

    Initial Commit

---
+++ Google AdMob Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Google AdMob Demo
+//
+//  Created by Pankaj on 02/01/19.
+//  Copyright © 2019 Canarys Automations Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
