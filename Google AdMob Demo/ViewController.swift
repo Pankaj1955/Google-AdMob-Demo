36c5fdaba53e3b6bb694350d848a2f208b157549
Pankajr <pankaj.ray@ecanarys.net>
2019-01-02 17:13:03 +0530

    test demo file

--- Google AdMob Demo/ViewController.swift
+++ Google AdMob Demo/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,UITextFieldDelegate, GADInterstitialDelegate{
+
+    @IBOutlet weak var GoogleBannerView: GADBannerView!
+    
+    var interstitialAd : GADInterstitial!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        GoogleBannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        GoogleBannerView.rootViewController = self
+        GoogleBannerView.load(GADRequest())
+        
+        self.interstitialAd = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        let request = GADRequest()
+        request.testDevices = ["2077ef9a63d2b398840261c8221a0c9b"]
+        self.interstitialAd.load(request)
+        self.interstitialAd = reloadInterstitialAd()
+
     }
 
-
+    @IBAction func showAd(_ sender: Any) {
+        if self.interstitialAd.isReady {
+            self.interstitialAd.present(fromRootViewController: self)
+        }
+    
+    }
+    func reloadInterstitialAd() -> GADInterstitial {
+        let interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        interstitial.delegate = self
+        interstitial.load(GADRequest())
+        return interstitial
+    }
+    
+    private func interstitialDidDismissScreen(ad: GADInterstitial!) {
+        self.interstitialAd = reloadInterstitialAd()
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+pod 'Google-Mobile-Ads-SDK'
+
+
+target 'Google AdMob Demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Google AdMob Demo
+
+end
